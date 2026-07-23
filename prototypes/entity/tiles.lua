502d0023e399b4c9593713502a91b4502b6335c0
Mathew Potrykus <mathew.potrykus@emcoruk.com>
2026-07-24 00:51:13 +0100

    - Updated Tiles to use function to determine the textures - Updated to work with 2.1 - Updated the Changelog

--- prototypes/entity/tiles.lua
+++ prototypes/entity/tiles.lua
@@ -22,26 +22,21 @@ require("__base__/prototypes/tile/tiles.lua")
 local modName = "__PoweredFloorExtended__"
 local tile_collision_masks = require("__base__/prototypes/tile/tile-collision-masks")
 local refined_concrete_sounds = sound_variations("__base__/sound/walking/refined-concrete", 11, 0.5)
-local tile_variants = {
-  main = {{picture = "__base__/graphics/terrain/concrete/concrete.png", count = 16, size = 1, scale = 0.5}},
-  transition = {inner_corner = {}, outer_corner = {}, side = {}, u_transition = {}},
-  transitions = {},
-  transitions_between_transitions = {},
-  overlay_layout = {
-    inner_corner = {spritesheet = "__base__/graphics/terrain/concrete/concrete-inner-corner.png", count = 16, scale = 0.5},
-    outer_corner = {spritesheet = "__base__/graphics/terrain/concrete/concrete-outer-corner.png", count = 8, scale = 0.5},
-    side = {spritesheet = "__base__/graphics/terrain/concrete/concrete-side.png", count = 16, scale = 0.5},
-    u_transition = {spritesheet = "__base__/graphics/terrain/concrete/concrete-u.png", count = 8, scale = 0.5},
-    o_transition = {spritesheet = "__base__/graphics/terrain/concrete/concrete-o.png", count = 4, scale = 0.5}
-  },
-  mask_layout = {
-    inner_corner = {spritesheet = "__base__/graphics/terrain/concrete/concrete-inner-corner-mask.png", count = 16, scale = 0.5},
-    outer_corner = {spritesheet = "__base__/graphics/terrain/concrete/concrete-outer-corner-mask.png", count = 8, scale = 0.5},
-    side = {spritesheet = "__base__/graphics/terrain/concrete/concrete-side-mask.png", count = 16, scale = 0.5},
-    u_transition = {spritesheet = "__base__/graphics/terrain/concrete/concrete-u-mask.png", count = 8, scale = 0.5},
-    o_transition = {spritesheet = "__base__/graphics/terrain/concrete/concrete-o-mask.png", count = 4, scale = 0.5}
+-- Helper function to generate unique variants for each tile type
+local function get_variants(style_name)
+  local copy = util.table.deepcopy(data.raw["tile"]["concrete"].variants)
+  copy.material_background = nil
+  copy.main = {
+    {
+      picture = modName .. "/graphic/flooring/" .. style_name .. "/tile.png",
+      count = 4,
+      scale = 0.125,
+      size = 1,
+      line_length = 2
+    }
   }
-}
+  return copy
+end
 
 data:extend({
   {
@@ -51,8 +46,9 @@ data:extend({
     collision_mask = tile_collision_masks.ground(),
     walking_speed_modifier = 2.0, vehicle_speed_modifier = 2.0,
     layer = 22, layer_group = "ground-artificial", decorative_removal_probability = 1.0,
-    variants = {main = {{picture = modName .. "/graphic/Flooring/" .. settings.startup["powered-flooring-style"].value .. "/tile.png", count = 4, scale = 0.125, size = 1, line_length = 2}}, transition = tile_variants},
-    transitions = concrete_transitions, transitions_between_transitions = concrete_transitions_between_transitions,
+    variants = get_variants(settings.startup["powered-flooring-style"].value),
+    transitions = data.raw["tile"]["concrete"].variants.transitions,
+    transitions_between_transitions = data.raw["tile"]["concrete"].variants.transitions_between_transitions,
     walking_sound = refined_concrete_sounds,
     map_color = {r = 192, g = 192, b = 192}, subgroup = "artificial-tiles", order = "a[power]"
   },
@@ -63,8 +59,9 @@ data:extend({
     collision_mask = tile_collision_masks.ground(),
     walking_speed_modifier = 2, vehicle_speed_modifier = 2,
     layer = 21, layer_group = "ground-artificial", decorative_removal_probability = 1.0,
-    variants = {main = {{picture = modName .. "/graphic/Flooring/" .. settings.startup["circuit-flooring-style"].value .. "/tile.png", count = 4, scale = 0.125, size = 1, line_length = 2}}, transition = tile_variants},
-    transitions = concrete_transitions, transitions_between_transitions = concrete_transitions_between_transitions,
+    variants = get_variants(settings.startup["circuit-flooring-style"].value),
+    transitions = data.raw["tile"]["concrete"].variants.transitions,
+    transitions_between_transitions = data.raw["tile"]["concrete"].variants.transitions_between_transitions,
     walking_sound = refined_concrete_sounds,
     map_color = {r = 192, g = 192, b = 192}, subgroup = "artificial-tiles", order = "b[circuit]"
   },
@@ -75,8 +72,9 @@ data:extend({
     collision_mask = tile_collision_masks.ground(),
     walking_speed_modifier = 0.9, vehicle_speed_modifier = 0.9,
     layer = 20, layer_group = "ground-artificial", decorative_removal_probability = 1.0,
-    variants = {main = {{picture = modName .. "/graphic/Flooring/" .. settings.startup["solar-flooring-style"].value .. "/tile.png", count = 4, scale = 0.125, size = 1, line_length = 2}}, transition = tile_variants},
-    transitions = concrete_transitions, transitions_between_transitions = concrete_transitions_between_transitions,
+    variants = get_variants(settings.startup["solar-flooring-style"].value),
+    transitions = data.raw["tile"]["concrete"].variants.transitions,
+    transitions_between_transitions = data.raw["tile"]["concrete"].variants.transitions_between_transitions,
     walking_sound = refined_concrete_sounds,
     map_color = {r = 10, g = 49, b = 94}, subgroup = "artificial-tiles", order = "c[solar]"
   },
@@ -87,8 +85,9 @@ data:extend({
     collision_mask = tile_collision_masks.ground(),
     walking_speed_modifier = 2, vehicle_speed_modifier = 2,
     layer = 23, layer_group = "ground-artificial", decorative_removal_probability = 1.0,
-    variants = {main = {{picture = modName .. "/graphic/Flooring/" .. settings.startup["network-flooring-style"].value .. "/tile.png", count = 4, scale = 0.125, size = 1, line_length = 2}}, transition = tile_variants},
-    transitions = concrete_transitions, transitions_between_transitions = concrete_transitions_between_transitions,
+    variants = get_variants(settings.startup["network-flooring-style"].value),
+    transitions = data.raw["tile"]["concrete"].variants.transitions,
+    transitions_between_transitions = data.raw["tile"]["concrete"].variants.transitions_between_transitions,
     walking_sound = refined_concrete_sounds,
     map_color = {r = 10, g = 49, b = 94}, subgroup = "artificial-tiles", order = "d[logistics]"
   }
